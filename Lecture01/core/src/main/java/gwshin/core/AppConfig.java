da0fd5d3a0e8b5400c95203f1221179891d18593
GwansuShin <happysun7080@gmail.com>
2022-07-20 15:04:06 +0900

    mod Lecture01 - AppConfig 스프링 기반으로 변경

--- Lecture01/core/src/main/java/gwshin/core/AppConfig.java
+++ Lecture01/core/src/main/java/gwshin/core/AppConfig.java
@@ -9,21 +9,30 @@ import gwshin.core.member.MemberServiceImpl;
 import gwshin.core.member.MemoryMemberRepository;
 import gwshin.core.order.OrderService;
 import gwshin.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// @Configutation: AppConfig에 설정을 구성
+@Configuration
 public class AppConfig {
 
+    // @Bean: Spring Container에 Spring Bean으로 등록
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
