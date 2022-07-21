15f6b0dcb33257659d528a471902881038534914
happysun7080 <happysun7080@naver.com>
2022-07-21 13:24:49 +0900

    mod Lecture01 - 탐색 위치와 기본 스캔 대상

--- Lecture01/core/src/main/java/gwshin/core/AutoAppConfig.java
+++ Lecture01/core/src/main/java/gwshin/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "gwshin.core",   // default: 현재 ComponentScan이 붙은 클래스의 위치
+        basePackageClasses = AutoAppConfig.class,
+
         // AppConfig 유지하기 위함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
