7e7ebff32c75f155d28b3c454b63240a3d5c50a0
happysun7080 <happysun7080@naver.com>
2022-07-21 15:11:10 +0900

    mod Lecture01 - 롬복(Lombok) 설정

--- Lecture01/core/src/main/java/gwshin/core/order/OrderServiceImpl.java
+++ Lecture01/core/src/main/java/gwshin/core/order/OrderServiceImpl.java
@@ -6,20 +6,22 @@ import gwshin.core.discount.RateDiscountPolicy;
 import gwshin.core.member.Member;
 import gwshin.core.member.MemberRepository;
 import gwshin.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 변수들을 매개 변수로 받는 생성자 만들어줌
 public class OrderServiceImpl implements  OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
