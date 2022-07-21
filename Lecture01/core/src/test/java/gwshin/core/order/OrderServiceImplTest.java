8bc2d789e58e757ea44d6187871f5a26310ba425
happysun7080 <happysun7080@naver.com>
2022-07-21 14:48:01 +0900

    mod Lecture01 - 생성자 주입

---
+++ Lecture01/core/src/test/java/gwshin/core/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package gwshin.core.order;
+
+import gwshin.core.discount.FixDiscountPolicy;
+import gwshin.core.member.Grade;
+import gwshin.core.member.Member;
+import gwshin.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
