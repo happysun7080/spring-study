73ba71a57c5f15c2e80cfba417bb0716d10f912c
happysun7080 <happysun7080@naver.com>
2022-08-01 10:17:36 +0900

    add Lecture03

---
+++ Lecture03/thymeleaf-basic/src/test/java/gwshin/thymeleaf/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package gwshin.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
