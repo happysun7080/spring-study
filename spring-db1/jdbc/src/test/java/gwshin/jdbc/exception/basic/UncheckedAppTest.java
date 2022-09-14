0c0fe78f9e9e61f0b6b16644ddede40b0a942be6
GwansuShin <happysun7080@naver.com>
2022-09-14 13:37:36 +0900

    add exception handling

---
+++ spring-db1/jdbc/src/test/java/gwshin/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,80 @@
+package gwshin.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+@Slf4j
+class UncheckedAppTest {
+
+    @Test
+    void unchecked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+//            e.printStackTrace();  // print 출력
+            log.info("ex", e);  // log 출력
+        }
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() throws RuntimeConnectException {
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSql();
+            } catch (SQLException e) {
+                throw new RuntimeSqlException(e);
+            }
+        }
+
+        public void runSql() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSqlException extends RuntimeException {
+        public RuntimeSqlException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
