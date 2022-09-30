f8e7644c9a0a406c813df777dee166e49c6abd95
GwansuShin <happysun7080@naver.com>
2022-09-30 17:20:41 +0900

    add JPA

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,24 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id  // 테이블의 PK와 해당 필드를 매핑한다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // PK 생성 값을 DB에서 생성하는 'IDENTITY' 방식을 사용한다.
     private Long id;
 
+    @Column(name = "item_name", length = 10)  // 객체의 필드를 테이블의 컬럼과 매핑한다.
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    /**
+     * JPA는 public 또는 protected의 기본 생성자가 필수이다.
+     */
     public Item() {
     }
 
