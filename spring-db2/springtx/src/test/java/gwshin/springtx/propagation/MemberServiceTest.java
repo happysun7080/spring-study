9f5a2c2f38baf40968449a63343975026f094ffb
happysun7080 <happysun7080@naver.com>
2022-12-12 14:43:13 +0900

    add 트랜잭션 전파 활용 - 커밋,롤백

---
+++ spring-db2/springtx/src/test/java/gwshin/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,66 @@
+package gwshin.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * memberService    @Transactional:OFF
+     * <p>
+     * memberRepository @Transactional:ON
+     * <p>
+     * logRepository    @Transactional:ON
+     */
+    @Test
+    void outerTxOff_success() {
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+
+    /**
+     * memberService    @Transactional:OFF
+     * <p>
+     * memberRepository @Transactional:ON
+     * <p>
+     * logRepository    @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        // given
+        String username = "로그예외_outerTxOff_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        // then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+}
